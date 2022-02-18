62ee023de5b7c2a8fc477649de15153539d16c8e
manojm01 <manojmetgud035@gmail.com>
2022-02-18 19:21:41 +0530

    heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash")
-const port = Process.env.PORT || 3000 
+const port = 4000 
 const mongoose = require('mongoose');
 require('./models/db');
 var Task = mongoose.model('Task');
@@ -103,6 +103,6 @@ app.get('/delete/:id', async(req,res)=>{
 })
 
 
-app.listen(port,()=>{  // do not add localhost here if you are deploying it
-  console.log("server listening to port "+port);
+app.listen(process.env.PORT || port, () => {
+  console.log('Express server started at port :'+port);
 });
